c2d43a7aab5e57e50e861a3e662eec9b7d5ceab6
Shaun Sullivan <shaunsully88@gmail.com>
2022-04-27 15:29:43 -0400

    Starting index page, have to resturcture it to ask questions correctly. Cuurently its generic EMployee, does not flow properly

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
 const inquirer = require('inquirer')
+const Employee = require('./lib/Employee.js');
 const Intern = require('./lib/Intern.js');
 const Engineer = require('./lib/Engineer.js');
-const Manager = require('./lib/Manager.js')
+const Manager = require('./lib/Manager.js');
+
+const employeeData = () => {
+    return inquirer
+    .prompt (
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the employee?'
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is the employee's id number?"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is the employee's email address?"
+    },
+    {
+        type: 'input',
+        name: 'role',
+        message: "What is the employee's role?"
+    }
+
+    
+    )
+}
